b342b42e7b007b7367fd761f375cf56393bd7414
rathodvamshi <vamshinayak756@gmail.com>
2024-04-17 13:05:13 +0530

    first commit

---
+++ learn.html/hai.py
@@ -0,0 +1,25 @@
+# STAR PATTERN ..............................>>>
+# n = int(input("enter number:"))
+# for i in range(n+1):
+#     for j in range(n-i+1):
+#         print(" ",end='')
+#     for j in range(0,i):
+#         print("*",end=" ")
+#     print()
+
+# reverses star pattern..............................<<<<>>>
+# n = int(input("enter"))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(" ",end='')
+#     for j in range(0,n-i):
+#         print("*",end=" ")
+#     print()
+
+
+# left sided pyramid ...................>>>>>>>>>>>>>>>>
+# n = int(input("enter"))
+# for i in range(n+1):
+#     for j in range(0,i):
+#         print("*",end=" ")
+#     print()
